10413beab49f99a12f4b329ff9b27580d7f12a00
TuNisiAa-guy <112637330+TuNisiAa-guy@users.noreply.github.com>
2024-03-25 21:13:39 +0100

    Update mouseFollower.js

--- mouseFollower.js
+++ mouseFollower.js
@@ -3,8 +3,10 @@ follower.className = "follower";
 document.getElementsByTagName("body")[0].appendChild(follower);
 
 let prevX = 0;
-
 let prevY = 0;
+let prevPrevY = 0;
+let prevPrevX = 0;
+
 
 document.addEventListener("mousemove", updateFollower);
 
@@ -12,12 +14,15 @@ function updateFollower(event){
     let x = event.clientX;
     let y = event.clientY;
 
-    follower.style.height = 20 + 20 * Math.log(Math.sqrt(Math.abs(x - prevX) + Math.abs(y - prevY))) + "px";
+    follower.style.height = 20 + 20 * Math.log(Math.sqrt(Math.abs(prevX - prevPrevX) + Math.abs(prevY - prevPrevY))) + "px";
     follower.style.opacity =
-    follower.style.transform = "rotate(" + Math.atan(-(x - prevX) / (y - prevY)) + "rad)";
-    follower.style.top = y - 10 + "px";
-    follower.style.left = x - 10 + "px";
-
+    follower.style.transform = "rotate(" + Math.atan(-(prevX - prevPrevX) / (prevY - prevPrevY)) + "rad)";
+    follower.style.top = prevY - 10 + "px";
+    follower.style.left = prevX - 10 + "px";
+    
+    prevPrevX = prevX;
+    prevPrevY = prevY;
+    
     prevX = x;
     prevY = y;
 }
