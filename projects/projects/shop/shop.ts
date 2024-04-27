c493a145265a263f15b136fde17540fd6c7cbf51
tunisiaa-guy <federico.toluzzo1@gmail.com>
2024-04-27 20:09:12 +0200

    more testing

--- projects/projects/shop/shop.ts
+++ projects/projects/shop/shop.ts
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 // @ts-ignore
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {Material, MeshBasicMaterial} from "three";
+import {roughness} from "three/examples/jsm/nodes/core/PropertyNode";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -11,8 +13,23 @@ document.body.appendChild( renderer.domElement );
 const loader = new GLTFLoader();
 loader.load('3dpea.gltf', (gltf) => {
     // Add the loaded model to your scene
-    scene.add(gltf.scene);
-    console.log('glTF: ', gltf);
+    const model = gltf.scene;
+    const material = new Material();
+    //material.
+    //model.material = material;
+    scene.add(model);
+
+    // Position the camera to view the model
+    camera.position.set(0, 0, 150); // Adjust as needed
+
+    // Add lights if necessary
+    const ambientLight = new THREE.RectAreaLight(0xffffff, 100);
+    ambientLight.position.set(-50, 70, 0);
+    scene.add(ambientLight);
+
+    // Render the scene
+    renderer.setClearColor(0xeeeeee); // Light gray background color
+    renderer.render(scene, camera);
+}, undefined, (error) => {
+    console.error('Error loading glTF model:', error);
 });
-
-
