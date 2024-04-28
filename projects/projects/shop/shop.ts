6b7aa2231ce653620469eb566c848faab20c6cfe
tunisiaa-guy <federico.toluzzo1@gmail.com>
2024-04-28 10:23:05 +0200

    3d preview now works

--- projects/projects/shop/shop.ts
+++ projects/projects/shop/shop.ts
@@ -2,34 +2,54 @@ import * as THREE from 'three';
 // @ts-ignore
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import {Material, MeshBasicMaterial} from "three";
-import {roughness} from "three/examples/jsm/nodes/core/PropertyNode";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth  - 16) / (window.innerHeight  - 16), 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth - 16, window.innerHeight  - 16);
 document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader();
+const controls = new OrbitControls( camera, renderer.domElement );
+animate();
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
+
+    renderer.render( scene, camera );
+
+}
 loader.load('3dpea.gltf', (gltf) => {
     // Add the loaded model to your scene
     const model = gltf.scene;
-    const material = new Material();
-    //material.
-    //model.material = material;
     scene.add(model);
 
     // Position the camera to view the model
     camera.position.set(0, 0, 150); // Adjust as needed
 
     // Add lights if necessary
-    const ambientLight = new THREE.RectAreaLight(0xffffff, 100);
+    const ambientLight = new THREE.RectAreaLight(0xFFFFFF, 10);
     ambientLight.position.set(-50, 70, 0);
+    ambientLight.width = 100;
+    ambientLight.height = 100;
+    ambientLight.lookAt(gltf.scene.position);
     scene.add(ambientLight);
 
+    const asd = new THREE.BoxGeometry(100, 1, 100, 1,1,1);
+    const as = new THREE.Mesh(asd);
+    as.position.set(-50, 70, 0);
+    scene.add(as);
+
     // Render the scene
+    scene.add(controls);
     renderer.setClearColor(0xeeeeee); // Light gray background color
     renderer.render(scene, camera);
+    camera.lookAt( gltf.scene.position );
 }, undefined, (error) => {
     console.error('Error loading glTF model:', error);
 });
+
