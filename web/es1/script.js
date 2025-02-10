718ff7e3f60816692e8374f3d308dae53f801f70
federico.toluzzo <federico.toluzzo1@gmail.com>
2025-02-10 11:58:02 +0100

    fixato i colori

--- web/es1/script.js
+++ web/es1/script.js
@@ -1,29 +1,40 @@
 var ruota = document.getElementById("ruota");
+var rot = 0;
+var mouseX = 0;
+var mouseY = 0;
 localStorage.setItem("deg", (0).toString());
 setInterval(function () {
     localStorage.setItem("deg", (parseInt(localStorage.getItem("deg")) + 1).toString());
     ruota.style.transform = "rotate(" + localStorage.getItem("deg") + "deg)";
+    rot += 1;
+    movePanels();
 }, 10);
 for (var i = 0; i < 10; i++) {
     for (var j = 0; j < 10; j++) {
         var panel = document.createElement("div");
         panel.className = "perspective";
+        panel.style.background = "rgb(" + (i + j) * 28 + ", " + ((i + j) * 28 - 255) + ", " + (i + j) * 28 + ")";
         document.getElementsByClassName("center")[0].appendChild(panel);
     }
 }
 document.addEventListener("mousemove", function (mouse) {
+    mouseX = mouse.x;
+    mouseY = mouse.y;
+    movePanels();
+});
+function movePanels() {
     var items = document.getElementsByClassName("perspective");
     for (var i = 0; i < items.length; i++) {
         var rect = items.item(i).getBoundingClientRect();
         var centerX = rect.x + rect.width / 2;
         var centerY = rect.y + rect.height / 2;
-        var deltaX = mouse.x - centerX;
-        var deltaY = mouse.y - centerY;
+        var deltaX = mouseX - centerX;
+        var deltaY = mouseY - centerY;
         //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
         //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
-        var rotX = deltaY / -1;
-        var rotY = deltaX / 1;
+        var rotX = deltaY / -5;
+        var rotY = deltaX / 5;
         items.item(i).style.transformStyle = "preserve-3d";
-        items.item(i).style.transform = "perspective(1000px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+        items.item(i).style.transform = "perspective(50px) rotateX(".concat((rotX + rot).toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
     }
-});
+}
--- web/es1/script.ts
+++ web/es1/script.ts
@@ -1,9 +1,15 @@
 const ruota = document.getElementById("ruota");
+let rot = 0;
+let mouseX = 0;
+let mouseY = 0;
+
 localStorage.setItem("deg", (0).toString())
 
 setInterval(() => {
     localStorage.setItem("deg", (parseInt(localStorage.getItem("deg")) + 1).toString());
     ruota.style.transform = "rotate(" + localStorage.getItem("deg") + "deg)";
+    rot += 1;
+    movePanels();
 }, 10);
 
 
@@ -12,24 +18,31 @@ for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         const panel = document.createElement("div");
         panel.className = "perspective";
+        panel.style.background = "rgb(" + (i + j) * 28 + ", " + ((i + j) * 28 - 255) + ", " + (i + j) * 28 +")";
         document.getElementsByClassName("center")[0].appendChild(panel);
     }
 }
 
 document.addEventListener("mousemove", (mouse) =>  {
+    mouseX = mouse.x;
+    mouseY = mouse.y;
+    movePanels();
+});
+
+function movePanels(){
     const items = document.getElementsByClassName("perspective");
     for (let i= 0; i < items.length; i++) {
         const rect = items.item(i).getBoundingClientRect()
         const centerX = rect.x + rect.width / 2;
         const centerY = rect.y + rect.height / 2;
 
-        const deltaX = mouse.x - centerX;
-        const deltaY = mouse.y - centerY;
+        const deltaX = mouseX - centerX;
+        const deltaY = mouseY - centerY;
         //const rotY = deltaX > 0 ? Math.log(deltaX) * 2 : Math.log(-deltaX) * -2;
         //const rotX = deltaY > 0 ? Math.log(deltaY) * -2 : Math.log(-deltaY) * 2;
-        const rotX = deltaY / -1;
-        const rotY = deltaX / 1;
+        const rotX = deltaY / -5;
+        const rotY = deltaX / 5;
         items.item(i).style.transformStyle = "preserve-3d"
-        items.item(i).style.transform = "perspective(1000px) rotateX(".concat(rotX.toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
+        items.item(i).style.transform = "perspective(50px) rotateX(".concat((rotX + rot).toString(), "deg) rotateY(").concat(rotY.toString(), "deg)");
     }
-});
+}
